413e7aa9b50820a18be486ba66230e3cef92c3ec
Marius <privacyfirst555@proton.me>
2024-10-10 14:05:36 +0300

    Added a timer for TOTP to stop it from trying to login with a code that has already been used up.

--- scripts/background.js
+++ scripts/background.js
@@ -2,6 +2,7 @@ if( 'function' === typeof importScripts) {
   importScripts('../libraries/JS-OTP/dist/jsOTP.js');
   var last_login1 = 0;
   var last_login2 = 0;
+  var last_code_gen = 0;
 
   chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
     globalTabId = tabId;
@@ -19,9 +20,15 @@ if( 'function' === typeof importScripts) {
           )}
         else if (Date.now() - last_login2 > 1000 && tab.url && tab.url.includes("privacyidea")) {
           last_login2 = Date.now();
-          chrome.storage.local.get(["secret", "auto2FA"], function(data) {
+          chrome.storage.local.get(["secret", "auto2FA"], async function(data) {
             if(data.auto2FA)
             {
+              if(Date.now()-last_code_gen < 31*1000) {
+                chrome.tabs.sendMessage(tabId, { message: "background_to_content", login_stage: 2, waiting: true });
+                await new Promise(r => setTimeout(r, 31*1000-Date.now()%(30*1000)));
+
+              }
+              last_code_gen = Math.floor(Date.now()/(30*1000))*30*1000;;
               var totp1 = new jsOTP.totp(30, 6);
               if(data.secret != null)
                 var code = totp1.getOtp(data.secret);
--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -1,6 +1,6 @@
 (() => {
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { message, secretScan, login_stage, username, password, code } = obj;
+        const { message, secretScan, login_stage, username, password, code, waiting } = obj;
         console.log("caught message");
         if(message === "background_to_content") {
             if (login_stage === 1) {
@@ -8,8 +8,17 @@
                 document.getElementById('password').value=password;
                 document.querySelector("button[id='submit_button']").click();
             } else if (login_stage === 2) {
+                if(waiting) {
+                    var nIntervId = setInterval(function() {
+                        var timeLeft = Math.floor((30*1000-Date.now()%(30*1000))/1000);
+                        document.getElementById('otpHint').value="Waiting: " + timeLeft;
+                        if(timeLeft < 1)
+                            clearInterval(nIntervId);
+                    }, 1000);
+                } else {
                 document.getElementById('otpHint').value=code;
                 document.querySelector("input[id='submitButton']").click();
+                }
             }
             // else if (secretScan == 1) {
             //     if(document.getElementsByClassName('text-2xl font-medium font-sans border-b border-neutral-300 border-solid tracking-tight') != null)
